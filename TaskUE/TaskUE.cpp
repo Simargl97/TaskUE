8a59b9a9d85405af475d1284ea9766d2f2c5d9fe
sergie <simargll1997@gmail.com>
2024-07-29 01:11:53 +0300

    Task 17

--- TaskUE/TaskUE.cpp
+++ TaskUE/TaskUE.cpp
@@ -1,32 +1,33 @@
 #include <iostream>
-#include <ctime>
+#include <cmath>
 
-int main() {
-    const int N = 5; 
-    int array[N][N];
-    
-    for(int i = 0; i < N; ++i)
-        for(int j = 0; j < N; ++j)
-            array[i][j] = i + j;
-    
-    for(int i = 0; i < N; ++i) {
-        for(int j = 0; j < N; ++j)
-            std::cout << array[i][j] << " ";
-        std::cout << std::endl;
+class Vector
+{
+public:
+    Vector() : x(3), y(3), z(3)
+    {}
+    Vector(double x, double y, double z) : x(x), y(y), z(z)
+    {}
+
+    double Length() 
+    {
+        return std::sqrt(x*x + y*y + z*z);
+    }
+    void Show()
+    {
+        std::cout << '\n' << x << ' ' << y << ' ' << z;
     }
     
-    std::time_t t = std::time(0);
-    std::tm now;
-    localtime_s(&now, &t);
+private:
+    double x;
+    double y;
+    double z;
+};
 
-    int current_day = now.tm_mday;
+int main()
+{
+    Vector v;
+    v.Show();
+    std::cout << '\n' << v.Length();
     
-    int row_index = current_day % N;
-    int sum = 0;
-    for(int j = 0; j < N; ++j)
-        sum += array[row_index][j];
-
-    std::cout << "Sum of elements in row " << row_index << ": " << sum << std::endl;
-  
-    return 0;
 }
