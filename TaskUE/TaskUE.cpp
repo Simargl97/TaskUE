4f1f1583efc4e890d1358175f3c4b34a340f1203
sergie <simargll1997@gmail.com>
2024-08-02 22:28:08 +0300

    Task 19

--- TaskUE/TaskUE.cpp
+++ TaskUE/TaskUE.cpp
@@ -3,6 +3,8 @@
 #include <vector>
 #include <string>
 
+//TASK 18
+
 class Stack {
 private:
     std::vector<std::string> elements;
@@ -25,6 +27,8 @@ public:
     }
 };
 
+//TASK 17
+
 class Vector
 {
 public:
@@ -39,7 +43,7 @@ public:
     }
     void Show()
     {
-        std::cout << '\n' << x << ' ' << y << ' ' << z;
+        std::cout << '\n' << x << ' ' << y << ' ' << z ;
     }
     
 private:
@@ -48,10 +52,59 @@ private:
     double z;
 };
 
+// TASK 19
+
+class Animal {
+public:
+    virtual void Voice() { 
+        std::cout << "Animal voice!" << std::endl;
+    }
+};
+
+
+class Dog : public Animal {
+public:
+    void Voice() override {  
+        std::cout << "Woof!" << std::endl;
+    }
+};
+
+
+class Cat : public Animal {
+public:
+    void Voice() override {
+        std::cout << "Meow!" << std::endl;
+    }
+};
+
+
+class Bird : public Animal {
+public:
+    void Voice() override {
+        std::cout << "Chirp!" << std::endl;
+    }
+};
+
 int main()
 {
     Vector v;
     v.Show();
-    std::cout << '\n' << v.Length();
+    std::cout << '\n' << v.Length() << '\n';
+
+    std::vector<Animal*> animals; 
+
+    animals.push_back(new Dog()); 
+    animals.push_back(new Cat());
+    animals.push_back(new Bird());
+
+  
+    for(auto animal : animals) {
+        animal->Voice();
+    }
     
+    for(auto animal : animals) {
+        delete animal;
+    }
+
+    return 0;
 }
